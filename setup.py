0d1c26f21fa1168fcc9d792b9d6594db875f4663
dciampa <drewciampa@yahoo.com>
2019-01-10 14:29:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(
-    name='cookiecutter-pypackage',
+    name='transitive-citation',
     packages=[],
     version='0.1.0',
     description='Transitive Citation',
