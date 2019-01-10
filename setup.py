8ab2186368f88740ab5827aa66206f328d1ce1ea
dciampa <drewciampa@yahoo.com>
2019-01-10 14:23:03 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='cookiecutter-pypackage',
+    packages=[],
+    version='0.1.0',
+    description='Transitive Citation',
+    author='Kirill Tchernyshyov, Arfon Smith, and Drew Ciampa',
+    license='MIT',
+    author_email='ktcherny@gmail.com',
+    url='https://github.com/dciampa/transitivecitation',
+    keywords=['ads', 'citations', 'package', ],
+)
